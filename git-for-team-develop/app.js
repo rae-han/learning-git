e4c273fca68d76aa45f8687d22b0f38af017acb1
raehan <raehan.jeong@cj.net>
2023-11-28 00:17:27 +0900

    feat: 개인 github 토큰 테스트

--- git-for-team-develop/app.js
+++ git-for-team-develop/app.js
@@ -1,4 +1,4 @@
 console.log('git 여러 계정 잘 되는지 테스트')
 console.log('231119 새로운 깃허브 토큰 테스트')
 console.log('231123 회사 깃허브 토큰 테스트')
-console.log('231123 개인 깃허브 토큰 테스트')
+console.log('231123 개인 깃허브 토큰 테스트 1')
